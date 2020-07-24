82f7c752eb6384529d65ae1cedb42732e8dd9afe
Sanju <sanjusnath9645@gmail.com>
2020-07-24 14:28:34 +0530

    checkout.html page is set into dynamic,but cart.html and checkout.html are not yet linked

--- store/views.py
+++ store/views.py
@@ -25,5 +25,13 @@ def cart(request):
     return render(request,'store/cart.html',context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order,created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0,'get_order_items':0}
+
+    context = {'items':items,'order':order}
     return render(request,'store/checkout.html',context)
