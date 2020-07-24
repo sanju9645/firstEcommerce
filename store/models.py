de83209f4ecc8b5ceae02aba18b5b36225120ba1
Sanju <sanjusnath9645@gmail.com>
2020-07-24 13:31:59 +0530

    store.html page is set into dynamic,image fields are added

--- store/models.py
+++ store/models.py
@@ -14,9 +14,21 @@ class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False,null=True,blank=False)
-    #image
+    image = models.ImageField(null=True,blank=True)
+
     def __str__(self):
         return self.name
+        
+    #if image is not uploaded,it will create error when loading store,html page
+    #to avoid that create an exception 
+    #it will return null string if image is not in there
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
