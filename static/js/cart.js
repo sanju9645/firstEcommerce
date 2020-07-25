9817e8b998805625bad79566c45ab439b5076de3
Sanju <sanjusnath9645@gmail.com>
2020-07-25 18:43:35 +0530

    cookies added,design errors solved

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,39 +1,63 @@
-let updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart')
 
-for(let i=0;i<updateBtns.length;i++){
-    updateBtns[i].addEventListener('click',function(){
-        let productId = this.dataset.product
-        let action = this.dataset.action
-        console.log('productId :',productId,'Action :',action)
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product
+		var action = this.dataset.action
+		console.log('productId:', productId, 'Action:', action)
+		console.log('USER:', user)
 
-        console.log('USER :',user)
-        if(user=== 'AnonymousUser'){
-            console.log('Not logged in')
-        }else{
-            updateUserOrder(productId, action)
-        }
-    })
+		if (user == 'AnonymousUser'){
+			addCookieItem(productId, action)
+		}else{
+			updateUserOrder(productId, action)
+		}
+	})
 }
 
-
 function updateUserOrder(productId, action){
-    console.log('user is logged in,sending data..')
-    let url = '/update_item/'
+	console.log('User is authenticated, sending data...')
 
-    fetch(url,{
-        method :'POST',
-        headers:{
-            'Content-Type':'application/json',
-            'X-CSRFToken':csrftoken,
-        },
-        body:JSON.stringify({'productId': productId,'action':action})
-    })
+		var url = '/update_item/'
 
-    .then((response) =>{
-        return response.json()
-    })
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
+}
 
-    .then((data) =>{
-        console.log('data :',data)
-    })
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] = {'quantity':1}
+
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	
+	location.reload()
 }
